4d23364c22b48a631c0c3aca55e80cb52527c5f5
jordan <contact@creativecuckoo.com>
2020-11-26 17:24:06 +0000

    adds api util for graphql client to fetch data from cms

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { client } from './utils/api';
+import {gql} from 'graphql-request';
 import Header from './components/Header';
 
 function App() {
+  const query = gql`
+    {
+      items {
+        title
+        description
+        image {
+          url
+        }
+      }
+    }
+  `
+
+  client.request(query)
+    .then((data) => {
+      console.log(data);
+    })
+
   return (
     <div className="App">
       <Header />
