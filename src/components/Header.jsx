aeaf3d3dc1bd639b0b1b76d26ae0d616c1d5d23b
jordan <contact@creativecuckoo.com>
2020-12-05 23:07:46 +0000

    adds link and styling to main header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from '@reach/router';
 
 const Header = () => {
   const title = "Moments...";
 
   return (
-    <MainHeader>
-      {title}
-    </MainHeader>
+    <Link to="/">
+      <MainHeader>
+        {title}
+      </MainHeader>
+    </Link>  
   );
 };
 
 const MainHeader = styled.header`
   font-size: 3rem;
   font-family: 'Fredericka the Great', cursive;
+  display: flex;
+  margin-left: 1.5rem;
 `;
 
 export default Header;
